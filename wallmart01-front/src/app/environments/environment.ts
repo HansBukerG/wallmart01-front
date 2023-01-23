8a637e7179b2eef3dbe848d9f8d92be118934214
Hans Buker <hansbuker1993@gmail.com>
2023-01-23 02:43:09 -0300

    savepoint before pr and production set

---
+++ wallmart01-front/src/app/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    domain: "https://back-data01.fly.dev/search/"
+}
--- wallmart01-front/src/app/services/data.service.ts
+++ wallmart01-front/src/app/services/data.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http'
 import { Product } from '../interfaces/product.interface';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 
 
-const url = "http://localhost:8000/search/"
+// const url = "http://localhost:8000/search/"
+const url = "https://back-data01.fly.dev/search/"
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
-  constructor(private http: HttpClient) { }
+  private domain:string | undefined
+  constructor(private http: HttpClient) {
+    this.domain = environment.domain
+  }
 
   getOptions = () => {
     const headers: HttpHeaders = new HttpHeaders({
@@ -25,7 +29,7 @@ export class DataService {
   }
 
   get = (search:string): Observable<Product[]> => {
-    return this.http.get<Product[]>(url+search);
+    return this.http.get<Product[]>(this.domain+search);
   }
 
 }
