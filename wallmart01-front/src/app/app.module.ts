8c97cb84e1255c2b4d8fb9382629a387795fa107
Hans Buker <hansbuker1993@gmail.com>
2023-01-27 16:54:18 -0300

    refactor: changes on name behavior

--- wallmart01-front/src/app/app.module.ts
+++ wallmart01-front/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartMenuComponent } from './components/start-menu/start-menu.component';
 import { SalesComponent } from './pages/sales/sales.component';
-import { SaleDetailComponent } from './components/sale-detail/sale-detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ImgUrlPipe } from './pipes/img-url.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -13,17 +12,18 @@ import { ProductDepthDetailComponent } from './components/product-depth-detail/p
 import { DiscountPercentajePipe } from './pipes/discount-percentaje.pipe';
 import { PriceFormatPipe } from './pipes/price-format.pipe';
 import {NgxPaginationModule} from 'ngx-pagination';
+import { SaleListComponent } from './components/sale-list/sale-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StartMenuComponent,
     SalesComponent,
-    SaleDetailComponent,
     ImgUrlPipe,
     ProductDepthDetailComponent,
     DiscountPercentajePipe,
-    PriceFormatPipe
+    PriceFormatPipe,
+    SaleListComponent
   ],
   imports: [
     BrowserModule,
